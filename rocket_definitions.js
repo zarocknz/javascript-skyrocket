24e1bc6a57af9a630dcdf4eb7f30cbffe0d5eb6c
Jaque <jaquevanderberg@catalyst.net.nz>
2017-07-07 14:12:08 +1200

    Add more rockets

--- rocket_definitions.js
+++ rocket_definitions.js
@@ -6,7 +6,10 @@ var skyrockets = [
 		'innerRadius' : 70,
 		'numParticles' : 80,
 		'color1' : '0,255,0',
-		'color2' : '255,0,0'
+		'color2' : '255,0,0',
+		'particleRadius' : 5,
+		'xFunction' : 'cos',
+		'yFunction' : 'sin'
 	},
 	{
 		'name' : 'rocket2',
@@ -14,14 +17,75 @@ var skyrockets = [
 		'innerRadius' : 10,
 		'numParticles' : 20,
 		'color1' : '0,100,255',
-		'color2' : '255,0,0'
-	},
+		'color2' : '255,0,0',
+    'particleRadius' : 5,
+    'xFunction' : 'cos',
+    'yFunction' : 'sin'
+  },
 	{
 		'name' : 'rocket3',
 		'outerRadius' : 80,
 		'innerRadius' : 20,
 		'numParticles' : 20,
 		'color1' : '0,150,200',
-		'color2' : '255,0,100'
-	}
+		'color2' : '255,0,100',
+    'particleRadius' : 5,
+    'xFunction' : 'cos',
+    'yFunction' : 'sin'
+  },
+  {
+    'name' : 'rocket4',
+    'outerRadius' : 30,
+    'innerRadius' : 20,
+    'numParticles' : 20,
+    'color1' : '0,150,200',
+    'color2' : '255,102,0',
+    'particleRadius' : 5,
+    'xFunction' : 'sin',
+    'yFunction' : 'tan'
+  },
+  {
+    'name' : 'rocket5',
+    'outerRadius' : 200,
+    'innerRadius' : 100,
+    'numParticles' : 20,
+    'color1' : '255,247,0',
+    'color2' : '255,0,0',
+    'particleRadius' : 5,
+    'xFunction' : 'cos',
+    'yFunction' : 'sin'
+  },
+  {
+    'name' : 'rocket6',
+    'outerRadius' : 120,
+    'innerRadius' : 20,
+    'numParticles' : 200,
+    'color1' : '255,247,0',
+    'color2' : '255,0,100',
+    'particleRadius' : 5,
+    'xFunction' : 'cos',
+    'yFunction' : 'sin'
+  },
+  {
+    'name' : 'rocket7',
+    'outerRadius' : 220,
+    'innerRadius' : 40,
+    'numParticles' : 320,
+    'color1' : '207,3,227',
+    'color2' : '0,255,43',
+    'particleRadius' : 5,
+    'xFunction' : 'tan',
+    'yFunction' : 'cos'
+  },
+  {
+    'name' : 'rocket8',
+    'outerRadius' : 120,
+    'innerRadius' : 100,
+    'numParticles' : 320,
+    'color1' : '207,3,227',
+    'color2' : '0,255,43',
+    'particleRadius' : 5,
+    'xFunction' : 'cos',
+    'yFunction' : 'sin'
+  },
 ];
